d50593d1431ae4863ed92975f73c49736426f0f3
Zihan Wei <zihan66@tamu.edu>
2017-10-12 18:23:21 -0500

    Text: Reverse

---
+++ ReverseString.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+  string s;
+  getline(cin,s);
+  for (int i=0; i <=(s.size()-1)/2;++i){
+    char temp=s[i];
+    s[i]=s[s.size()-i-1];
+    s[s.size()-i-1]=temp;
+  }
+  cout << s ;
+
+  return 0;
+}
