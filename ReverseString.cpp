c1b787904369f92df18ea2025bbc11c7d6ac0814
Zihan Wei <zihan66@tamu.edu>
2017-10-13 22:25:40 -0500

    improve for loop

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   string s;
   getline(cin,s);
-  for (int i=0; i <=(s.size()-1)/2;++i){
+  for (int i=0; i < s.size()/2;++i){
     char temp=s[i];
     s[i]=s[s.size()-i-1];
     s[s.size()-i-1]=temp;
